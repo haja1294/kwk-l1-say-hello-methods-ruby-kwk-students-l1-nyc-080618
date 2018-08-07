1d1d6fe39991b28e4045c419a1dfe540b961b01e
Haja Trawally <hajatrawally6@gmail.com>
2018-08-07 14:55:25 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
-def say_hello(greeting="Hi",your_name="Ruby Programmer)
-  puts "#{greeting} #{your name}"
+def say_hello (greeting="Hi", your_name="Ruby Programmer)
+  puts "#{greeting} #{your name}""
 end 
 
 say_hello("Hola","Haja")
